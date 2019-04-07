a62a76bf8f9c31c0b82cdd52d035e76cfd3c8a4c
Vladislav Molchanov <Molchanov96@yandex.ru>
2019-04-07 22:56:47 +0300

    Заменил BrowserRouter на HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import React, {Component} from 'react';
 import Welcome from './components/Welcome';
 import Mode from './components/Mode';
