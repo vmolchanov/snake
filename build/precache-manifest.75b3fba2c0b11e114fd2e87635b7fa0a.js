854e8cac03846cfc54f35bf87e06bbeca5a2e08a
Vladislav Molchanov <Molchanov96@yandex.ru>
2019-04-07 22:10:53 +0300

    Поправил gitignore. Добавил сборку

---
+++ build/precache-manifest.75b3fba2c0b11e114fd2e87635b7fa0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b25ef2140f8578199958",
+    "url": "/static/js/main.9af6c32a.chunk.js"
+  },
+  {
+    "revision": "d7fe64de723c5ffb9074",
+    "url": "/static/js/2.b54fa6e3.chunk.js"
+  },
+  {
+    "revision": "b25ef2140f8578199958",
+    "url": "/static/css/main.e35e1aa4.chunk.css"
+  },
+  {
+    "revision": "d7fe64de723c5ffb9074",
+    "url": "/static/css/2.fd21b938.chunk.css"
+  },
+  {
+    "revision": "0caa9c323b939cea612c680b8edb0b42",
+    "url": "/index.html"
+  }
+];
