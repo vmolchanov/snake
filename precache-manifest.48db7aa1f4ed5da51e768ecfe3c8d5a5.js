6e46d5a79a73711dd23cab1db4e2660ad3ff4d72
Vladislav Molchanov <Molchanov96@yandex.ru>
2019-04-07 22:34:35 +0300

    Updates

---
+++ precache-manifest.48db7aa1f4ed5da51e768ecfe3c8d5a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28fbb7debeb67f585d77",
+    "url": "/snake/static/js/runtime~main.ccc81976.js"
+  },
+  {
+    "revision": "b25ef2140f8578199958",
+    "url": "/snake/static/js/main.9af6c32a.chunk.js"
+  },
+  {
+    "revision": "d7fe64de723c5ffb9074",
+    "url": "/snake/static/js/2.b54fa6e3.chunk.js"
+  },
+  {
+    "revision": "b25ef2140f8578199958",
+    "url": "/snake/static/css/main.e35e1aa4.chunk.css"
+  },
+  {
+    "revision": "d7fe64de723c5ffb9074",
+    "url": "/snake/static/css/2.fd21b938.chunk.css"
+  },
+  {
+    "revision": "6d75bdc69b72d0c03bad3a08f7e53d68",
+    "url": "/snake/index.html"
+  }
+];
